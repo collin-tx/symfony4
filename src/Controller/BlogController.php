4c1226f983ff86fd27e624d7235d78a443954ea0
Collin Smith <cmsmith.tx@gmail.com>
2020-02-24 17:00:28 -0600

    AbstractController, Request, Response

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -34,11 +35,13 @@ class BlogController extends AbstractController
    * @Route("/{page}", name="blog_list")
    */
 
-  public function list($page = 5)
+  public function list($page = 1, Request $request)
   {
-    return new JsonResponse(
+    $limit = $request->get('limit', 10);
+    return $this->json(
       [
         'page' => $page,
+        'limit' => $limit,
         'data' => array_map(function ($item) {
           return $this->generateUrl('blog_by_id', ['id' => $item['id']]);
         }, self::POSTS)
@@ -51,7 +54,7 @@ class BlogController extends AbstractController
    */
   public function post($id)
   {
-    return new JsonResponse(
+    return $this->json(
       self::POSTS[array_search($id, array_column(self::POSTS, 'id'))]
     );
   }
@@ -61,7 +64,7 @@ class BlogController extends AbstractController
    */
   public function postBySlug($slug)
   {
-    return new JsonResponse(
+    return $this->json(
       self::POSTS[array_search($slug, array_column(self::POSTS, 'slug'))]
     );
   }
